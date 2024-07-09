0e069fc6163d3eb8fac9fcb12d227747d0ac73b6
Daniel Hussey <daniel.r.hussey@gmail.com>
2024-07-09 22:13:56 +1000

    Updates

--- app/views/layouts/application_layout.rb
+++ app/views/layouts/application_layout.rb
@@ -12,13 +12,12 @@ class ApplicationLayout < ApplicationView
 				meta name: "viewport", content: "width=device-width,initial-scale=1"
 				csp_meta_tag
 				csrf_meta_tags
-				# stylesheet_link_tag "application", data_turbo_track: "reload"
-				# javascript_importmap_tags
-
-				render Shared::Navbar.new
+				stylesheet_link_tag "application", data_turbo_track: "reload"
+				javascript_importmap_tags
 			end
 
 			body do
+				render Shared::Navbar.new
 				main(&block)
 			end
 		end
